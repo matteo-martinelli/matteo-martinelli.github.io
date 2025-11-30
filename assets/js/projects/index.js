5dc5122b4f8db911c9db1098507574adde848d1c
Matteo Martinelli <wmatte2005@gmail.com>
2025-11-30 22:23:09 +0100

    Publications and Projects pages setup
    Added automatic pages setup through js scripts.

--- assets/js/projects/index.js
+++ assets/js/projects/index.js
@@ -1,21 +1,29 @@
-function toTitleCase(str) {
-  return str
-    .toLowerCase()                 // tutto minuscolo
-    .split(' ')                   // dividi in parole
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))  // prima lettera maiuscola + resto minuscolo
-    .join(' ');                   // riunisci le parole
+// Pojects Page Script
+
+function addDebuggingGraphhicalElements(debugging_value) {
+    // Function aimed at activate the debugging mode fo the UI
+    if (debugging_value){
+        var industrial_projects_section = document.getElementById('industrial-projects');
+        industrial_projects_section.style.border = '1px solid red';
+    
+        var projects_table = document.getElementById('projects-table');
+        projects_table.style.border = '1px solid green';
+    }
 }
 
-var section = document.getElementById('db-content-test-section');
-console.log('Element returned:');
-console.log(section);
 
-var counter = 0;
+// TODO: collect in a separate utils.js file.
+function toTitleCase(str) {
+  return str
+    .toLowerCase()                                              // all lower case
+    .split(' ')                                                 // divide words
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))  // first letter uppercase + other lowercase
+    .join(' ');                                                 // merge words
+}
+
 
 fetch('/assets/db/projects.json').then(
     response => {
-        console.log('response status:\n', response.status);
-        console.log('response:\n', response);
         if (!response.ok) {
             throw new Error('There was an error reading the file!');
         } 
@@ -24,36 +32,41 @@ fetch('/assets/db/projects.json').then(
     .then(values =>{
         for (var key in values){
             switch (key){
-                case 'industrial-projects':
-                    // TODO: give an id and a class to the created div
-                    const industrial_projects_div = document.createElement('div');
-                    const industrial_projects_title = document.createElement('h2');
-                    const industrial_projects_table = document.createElement('table')
-                    industrial_projects_table.id = 'projects-table2';
+                case 'industrial-projects': {
+                    var industrial_projects_section = document.getElementById('industrial-projects');
+                    
+                    var industrial_projects_div = document.createElement('div');
+                    var industrial_projects_title = document.createElement('h2');
                     industrial_projects_title.textContent = 'Industrial Projects:'; 
                     industrial_projects_div.appendChild(industrial_projects_title);
+
+                    var industrial_projects_table = document.createElement('table')
+                    industrial_projects_table.id = 'projects-table';
+
                     for (var key_industrial in values[key]) {
                         var content = values[key][key_industrial];
                         var description = content['title'];
-                        const table_row = document.createElement('tr');
-                        const table_entry = document.createElement('td');
-                        const table_separator = document.createElement('td');
+                        var table_row = document.createElement('tr');
+                        var table_entry = document.createElement('td');
+                        var table_separator = document.createElement('td');
                         table_separator.className = 'separator';
-                        const table_value = document.createElement('td');
-                        const ul = document.createElement('ul');
-                        const li = document.createElement('li');
+                        var table_value = document.createElement('td');
+                        var ul = document.createElement('ul');
+                        var li = document.createElement('li');
                         for (var elem in values[key][key_industrial]["links"]) {
-                            const link_elem = document.createElement('a');
+                            var link_elem = document.createElement('a');
                             link_elem.className = 'link';
                             link_elem.href = values[key][key_industrial][elem];
                             link_elem.target = '_blank';
                             link_elem.rel = 'noopener noreferrer';
                             link_elem.textContent = elem.replace('-link', '').replaceAll('-', ' ');
                             table_value.appendChild(link_elem);
-                            const separator = document.createTextNode(', ');
+                            var separator = document.createTextNode(', ');
                             table_value.appendChild(separator);
                         }
                         table_value.removeChild(table_value.lastChild);
+
+                        ul.style.paddingLeft = '1em';
 
                         li.textContent = description;
                         ul.appendChild(li);
@@ -65,66 +78,33 @@ fetch('/assets/db/projects.json').then(
                         industrial_projects_table.appendChild(table_row);
                     }
                     industrial_projects_div.appendChild(industrial_projects_table);                        
-                    section.appendChild(industrial_projects_div);
-                    break;
+                    industrial_projects_section.appendChild(industrial_projects_div);
                     
-                case 'scientific-articles':
-                    //console.log(values[key]);
-                    const scientific_articles_div = document.createElement('div');
-                    const scientific_articles_title = document.createElement('h2');
-                    const scientific_articles_table = document.createElement('table');
-                    scientific_articles_table.id = 'scientific-articles-table2';
-                    scientific_articles_title.textContent = 'Scientific Articles:'; 
-                    scientific_articles_div.id = 'scientific-articles';
-                    scientific_articles_div.appendChild(scientific_articles_title);
-                    var counter = 0;
-                    for (var key_industrial in values[key]) {
-                        counter = counter + 1;
-                        console.log(counter);
-                        var content = values[key][key_industrial];
-                        //console.log(content);
-                        var type = content['type'];
-                        var title = content['title'];
-                        var authors = content['authors'];
-                        var publisher = content['publisher'];
-                        var journal_issue_conference = content['journal-issue-conference'];
-                        var year = content['year'];
-                        var doi_link = content['reference-doi-link'];
-
-                        const table_row = document.createElement('tr');
-                        table_row.className = 'scientific-article-row';
-                        const table_type = document.createElement('td');
-                        const table_title = document.createElement('td');
-                        const table_authors = document.createElement('td');
-                        const table_publisher = document.createElement('td');
-                        const table_journal_issue_conference = document.createElement('td');
-                        const table_year = document.createElement('td');
-                        const table_doi_link = document.createElement('td');
-                        const ul = document.createElement('ul');
-                        const li = document.createElement('li');
-
-                        li.textContent = type;
-                        ul.appendChild(li);
-                        table_type.appendChild(ul);
-                        table_title.textContent = title;
-                        table_authors.textContent = authors;
-                        table_publisher.textContent = publisher;
-                        table_journal_issue_conference.textContent = journal_issue_conference;
-                        table_year.textContent = year;
-                        table_doi_link.textContent = doi_link;
-                        //table_type.textContent = type; 
-                        table_row.appendChild(table_type);
-                        table_row.appendChild(table_title);
-                        table_row.appendChild(table_authors);
-                        table_row.appendChild(table_publisher);
-                        table_row.appendChild(table_journal_issue_conference);
-                        table_row.appendChild(table_year);
-                        table_row.appendChild(table_doi_link);
-                        scientific_articles_table.appendChild(table_row);
+                    var industrial_projects_table_tr = industrial_projects_table.getElementsByTagName('tr');
+                    for (var i = 0; i < industrial_projects_table_tr.length; i++) {
+                        var industrial_projects_table_td = industrial_projects_table_tr[i].getElementsByTagName('td');
+                        for (var j = 0; j < industrial_projects_table_td.length; j++) {
+                            industrial_projects_table_td[j].style.paddingBottom = '0.4em';
+                            if (industrial_projects_table_td[j].className == 'separator') {
+                                industrial_projects_table_td[j].style.paddingLeft = '15px';
+                                industrial_projects_table_td[j].style.paddingRight = '15px';
+                            }
+                        }
+                        industrial_projects_table_tr[i].style.border = '1px solid green';
                     }
-                    scientific_articles_div.appendChild(scientific_articles_table);
-                    section.appendChild(scientific_articles_div);
+
+                    var table_separator = document.querySelector('#projects-table, .separator');
+                    
+                    industrial_projects_section.style.paddingTop = '10px';
+                    industrial_projects_section.style.paddingBottom = '10px';
+                    industrial_projects_section.style.paddingLeft = '25px';
+
                     break;
+                }
             }
         }
     })
+    .then(values => {
+        addDebuggingGraphhicalElements(false);
+    })
+
---
+++ assets/js/publications/index.js
@@ -0,0 +1,211 @@
+// Publications Page Script
+
+function addDebuggingGraphhicalElements(debugging_value) {
+    // Function aimed at activate the debugging mode fo the UI
+    if (debugging_value){    
+        var scientific_articles_section = document.getElementById('scientific-articles');
+        scientific_articles_section.style.border = '1px solid orange';
+
+        var university_thesis_section = document.getElementById('university-thesis');
+        university_thesis_section.style.border = '1px solid olive';
+    }
+}
+
+
+// TODO: collect in a separate utils.js file
+function toTitleCase(str) {
+  return str
+    .toLowerCase()                                              // all lower case
+    .split(' ')                                                 // divide words
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))  // first letter uppercase + other lowercase
+    .join(' ');                                                 // merge words
+}
+
+
+fetch('/assets/db/publications.json').then(
+    response => {
+        if (!response.ok) {
+            throw new Error('There was an error reading the file!');
+        } 
+        return response.json();
+    })
+    .then(values =>{
+        for (var key in values){
+            switch (key){                   
+                // TODO: improve vertical spacing between publications 
+                case 'scientific-articles':{
+                    var scientific_articles_section = document.getElementById('scientific-articles');
+                    
+                    var scientific_articles_h2_title = document.createElement('h2');   // Add class, id, ..-
+                    scientific_articles_h2_title.textContent = 'Publications';
+                    scientific_articles_section.appendChild(scientific_articles_h2_title);
+                    
+                    var sa_ol = document.createElement('ol');         // Add class, id, ..-
+                    scientific_articles_section.appendChild(sa_ol);
+                                        
+                    for (var key_industrial in values[key]) {
+                        // Retrieve data to present
+                        var content = values[key][key_industrial];
+                        var type = content['type'];
+                        var title = content['title'];
+                        var authors = content['authors'];
+                        var publisher = content['publisher'];
+                        var journal_issue_conference = content['journal-issue-conference'];
+                        var year = content['year'];
+                        var doi_link = content['reference-doi-link'];
+                        
+                        // Create title and badge structure (li)
+                        var li_element = document.createElement('li');
+                        li_element.style.paddingBottom = '0.4em';
+                        
+                        // Title and Badge
+                        var title_element = document.createElement('h4');
+                        var title_text_node = document.createTextNode(title);
+                        
+                        var badge_element = document.createElement('a');
+                        var badge_text_node = document.createTextNode(type);
+                        badge_element.appendChild(badge_text_node);
+                        
+                        if (type == 'journal') {     // Enum all the possible types
+                            badge_element.classList.add('badge', 'journal');
+                        } 
+                        else if (type == 'conference') {
+                            badge_element.classList.add('badge', 'conference');
+                        }
+                        else if (type == 'preprint') {
+                            badge_element.classList.add('badge', 'preprint');
+                        }
+                        
+                        title_element.appendChild(title_text_node);
+                        title_element.appendChild(badge_element);
+                        li_element.appendChild(title_element);
+
+                        // Create subelements structure (ol)
+                        var ul = document.createElement('ul');
+                        
+                        // Authors
+                        var li_authors = document.createElement('li');
+                        var authors_text_node = document.createTextNode(authors);
+                        authors_emph_element = document.createElement('i');
+                        authors_emph_element.appendChild(authors_text_node);
+                        li_authors.appendChild(authors_emph_element); 
+                        ul.appendChild(li_authors);
+
+                        // Journal / Conference
+                        var li_publisher = document.createElement('li');
+                        var publisher_content = '(' + publisher + ') ' + journal_issue_conference + ', ' + year;
+                        li_publisher.textContent = publisher_content; 
+                        ul.appendChild(li_publisher);
+
+                        // DOI
+                        var li_doi = document.createElement('li');
+                        li_doi.textContent = 'DOI: ';
+                        var doi_link_a = document.createElement('a');
+                        doi_link_a.textContent = doi_link; 
+                        doi_link_a.href = doi_link;
+                        doi_link_a.target = '_blank';
+                        doi_link_a.rel = 'noopener noreferrer';
+                        doi_link_a.style.color = 'orange';
+                        li_doi.appendChild(doi_link_a); 
+                        
+                        // Append the build li to the ul
+                        ul.appendChild(li_doi);
+                        ul.style.marginLeft = '0.9em';
+                        
+                        // Append the ul to the element in process
+                        li_element.appendChild(ul);
+
+                        // Append the result to the list grouping scientific articles
+                        sa_ol.append(li_element);
+                        sa_ol.style.paddingLeft = '1.1em';
+                    }
+
+                    scientific_articles_section.style.paddingTop = '10px';
+                    scientific_articles_section.style.paddingBottom = '10px';
+                    scientific_articles_section.style.paddingLeft = '25px';
+
+                    break;
+                }
+
+                case 'university-thesis': {
+                    var university_thesis_section = document.getElementById('university-thesis');
+                    
+                    var section_title = document.createElement('h2');
+                    section_title.textContent = 'University Thesis';
+                    university_thesis_section.appendChild(section_title);
+                    
+                    var ordered_elements_list = document.createElement('ol');
+
+                    for (var table_key in values[key]) {
+                        var content = values[key][table_key];
+                        var degree = content['degree'];
+                        var course = content['course'];
+                        var title = content['title'];
+                        var authors = content['authors'];
+                        var publisher = content['publisher'];
+                        var year = content['year'];
+                        var doi_link = content['reference-doi-link'];
+
+                        // Title and Badge
+                        var title_text_node = document.createTextNode(title);
+                        var title_element = document.createElement('h4');
+                        var li_title = document.createElement('li');
+                        title_element.appendChild(title_text_node);
+                        li_title.appendChild(title_element);
+                        li_title.style.paddingBottom = '0.4em'; // TODO: improve, could be the opposite
+                        
+                        var badge_element = document.createElement('a');
+                        var badge_text_node = document.createTextNode(table_key);
+                        badge_element.appendChild(badge_text_node);
+                        title_element.appendChild(badge_element);
+                        
+                        if (table_key == 'phd-thesis') {     // Enum all the possible types
+                            badge_element.classList.add('badge', 'phd-thesis');
+                        } 
+                        else if (table_key == 'masters-thesis') {
+                            badge_element.classList.add('badge', 'masters-thesis');
+                        }
+
+                        // Degree, course, university and year setting
+                        var string_content = degree + ' in ' + course + ', ' + publisher + ', ' + year;
+                        var degree_course_publisher_year_text_node = document.createTextNode(string_content);
+                        var li_degree_course_publisher_year = document.createElement('li');
+                        li_degree_course_publisher_year.appendChild(degree_course_publisher_year_text_node);
+                        
+                        // Link setting
+                        var li_link = document.createElement('li');
+                        li_link.textContent = 'Link: ';
+                        var link_a = document.createElement('a');
+                        link_a.textContent = doi_link; 
+                        link_a.href = doi_link;
+                        link_a.target = '_blank';
+                        link_a.rel = 'noopener noreferrer';
+                        link_a.style.color = 'orange';
+                        li_link.appendChild(link_a);
+                        
+                        // Appending elements to appropriate lists and styling
+                        var unordered_artifact_description = document.createElement('ul');
+                        unordered_artifact_description.appendChild(li_degree_course_publisher_year);
+                        unordered_artifact_description.appendChild(li_link);
+                        unordered_artifact_description.style.marginLeft = '0.9em';
+
+                        ordered_elements_list.appendChild(li_title);
+                        ordered_elements_list.appendChild(unordered_artifact_description);
+                        ordered_elements_list.style.paddingLeft = '1.1em';
+
+                    }
+                    // Appending elements to appropriate lists and styling
+                    university_thesis_section.appendChild(ordered_elements_list);
+                    university_thesis_section.style.paddingTop = '10px';
+                    university_thesis_section.style.paddingBottom = '10px';
+                    university_thesis_section.style.paddingLeft = '25px';
+                
+                    break;
+                }
+            }
+        }
+    })
+    .then(values => {
+        addDebuggingGraphhicalElements(false);
+    })
+

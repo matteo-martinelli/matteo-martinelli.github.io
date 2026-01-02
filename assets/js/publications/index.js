f038c3e26ebb3a244d84859c14311d98de650ef4
Matteo Martinelli <wmatte2005@gmail.com>
2026-01-02 17:40:46 +0100

    Publication css stylesheet updated
    Style before defined in the JS mockup has been moved
    in the proper CSS file. Moreover, the JS has been polished.

--- assets/js/publications/index.js
+++ assets/js/publications/index.js
@@ -36,17 +36,22 @@ fetch('/assets/db/publications.json').then(
                 case 'scientific-articles':{
                     var scientific_articles_section = document.getElementById('scientific-articles');
                     
-                    var scientific_articles_h2_title = document.createElement('h2');   // Add class, id, ..-
-                    scientific_articles_h2_title.textContent = 'Publications';
-                    scientific_articles_section.appendChild(scientific_articles_h2_title);
+                    var research_articles_h2_title = document.createElement('h2');   // Add class, id, ..-
+                    research_articles_h2_title.textContent = 'Publications';
+                    scientific_articles_section.appendChild(research_articles_h2_title);
                     
-                    var sa_ol = document.createElement('ol');         // Add class, id, ..-
-                    scientific_articles_section.appendChild(sa_ol);
+                    var research_publications_ol = document.createElement('ol');         // research_publications_ol
+                    research_publications_ol.className = 'scientific-publications-list';
+                    research_publications_ol.id = 'scientific-articles-list';
+                    scientific_articles_section.appendChild(research_publications_ol);
                                         
-                    for (var key_industrial in values[key]) {
-                        // TODO: reorganize in a dictionary and build a brief algorithm
+                    for (var pub_details in values[key]) {
+                        // List item where to store scientific publication details
+                        var research_publications_ol_li = document.createElement('li');
+                        
                         // Retrieve data to present
-                        var content = values[key][key_industrial];
+                        // TODO: reorganize in a dictionary and build a brief algorithm
+                        var content = values[key][pub_details];
                         var type = content['type'];
                         var title = content['title'];
                         var authors = content['authors'];
@@ -54,18 +59,14 @@ fetch('/assets/db/publications.json').then(
                         var journal_issue_conference = content['journal-issue-conference'];
                         var year = content['year'];
                         var doi_link = content['reference-doi-link'];
-                        
-                        // Create title and badge structure (li)
-                        var li_element = document.createElement('li');
-                        li_element.style.paddingBottom = '0.4em';
-                        
+
                         // Title and Badge
                         var title_element = document.createElement('h4');
-                        var title_text_node = document.createTextNode(title);
+                        title_element.textContent = title;
                         
                         var badge_element = document.createElement('a');
-                        var badge_text_node = document.createTextNode(type);
-                        badge_element.appendChild(badge_text_node);
+                        badge_element.textContent = type;
+                        title_element.appendChild(badge_element);
                         
                         if (type == 'journal') {     // Enum all the possible types
                             badge_element.classList.add('badge', 'journal');
@@ -77,54 +78,46 @@ fetch('/assets/db/publications.json').then(
                             badge_element.classList.add('badge', 'preprint');
                         }
                         
-                        title_element.appendChild(title_text_node);
-                        title_element.appendChild(badge_element);
-                        li_element.appendChild(title_element);
+                        research_publications_ol_li.appendChild(title_element);
 
-                        // Create subelements structure (ol)
-                        var ul = document.createElement('ul');
+                        // Create subelements structure of ol 
+                        // reporting scientific publications information in an ul
+                        var research_pub_description_ul = document.createElement('ul');
+                        research_pub_description_ul.className = 'scientific-publication-details';
+                        research_pub_description_ul.id = 'scientific-publication-details-ul';
                         
                         // Authors
-                        var li_authors = document.createElement('li');
-                        var authors_text_node = document.createTextNode(authors);
-                        authors_emph_element = document.createElement('i');
-                        authors_emph_element.appendChild(authors_text_node);
-                        li_authors.appendChild(authors_emph_element); 
-                        ul.appendChild(li_authors);
+                        var research_pub_description_ul_li = document.createElement('li');
+                        var authors_emph_element = document.createElement('i');
+                        authors_emph_element.textContent = authors;
+                        research_pub_description_ul_li.appendChild(authors_emph_element);
+                        // Append the built li to the research_pub_description_ul
+                        research_pub_description_ul.appendChild(research_pub_description_ul_li);
 
                         // Journal / Conference
-                        var li_publisher = document.createElement('li');
+                        var research_pub_description_ul_li = document.createElement('li');
                         var publisher_content = '(' + publisher + ') ' + journal_issue_conference + ', ' + year;
-                        li_publisher.textContent = publisher_content; 
-                        ul.appendChild(li_publisher);
+                        research_pub_description_ul_li.textContent = (publisher_content);
+                        // Append the built li to the research_pub_description_ul
+                        research_pub_description_ul.appendChild(research_pub_description_ul_li);
 
                         // DOI
-                        var li_doi = document.createElement('li');
-                        li_doi.textContent = 'DOI: ';
+                        var research_pub_description_ul_li = document.createElement('li');
+                        research_pub_description_ul_li.textContent = 'DOI: ';
                         var doi_link_a = document.createElement('a');
                         doi_link_a.textContent = doi_link; 
                         doi_link_a.href = doi_link;
                         doi_link_a.target = '_blank';
                         doi_link_a.rel = 'noopener noreferrer';
                         doi_link_a.style.color = 'orange';
-                        li_doi.appendChild(doi_link_a); 
+                        research_pub_description_ul_li.appendChild(doi_link_a); 
+                        research_pub_description_ul.appendChild(research_pub_description_ul_li);
                         
-                        // Append the build li to the ul
-                        ul.appendChild(li_doi);
-                        ul.style.marginLeft = '0.9em';
-                        
-                        // Append the ul to the element in process
-                        li_element.appendChild(ul);
-
-                        // Append the result to the list grouping scientific articles
-                        sa_ol.append(li_element);
-                        sa_ol.style.paddingLeft = '1.1em';
+                        // Append the research_pub_description_ul to the upper-level li element in process
+                        research_publications_ol_li.appendChild(research_pub_description_ul);
+                        // Append the finally built research_publications_ol_li to the list grouping research articles
+                        research_publications_ol.append(research_publications_ol_li);
                     }
-
-                    scientific_articles_section.style.paddingTop = '10px';
-                    scientific_articles_section.style.paddingBottom = '10px';
-                    scientific_articles_section.style.paddingLeft = '25px';
-
                     break;
                 }
 
@@ -135,9 +128,18 @@ fetch('/assets/db/publications.json').then(
                     section_title.textContent = 'University Thesis';
                     university_thesis_section.appendChild(section_title);
                     
-                    var ordered_elements_list = document.createElement('ol');
-
+                    var university_publications_ol = document.createElement('ol');
+                    university_publications_ol.className = 'scientific-publications-list';
+                    university_publications_ol.id = 'university-articles-list';
+                    
+                    university_thesis_section.appendChild(university_publications_ol);
+                    
                     for (var table_key in values[key]) {
+                        // List item where to store university publication details
+                        var university_publications_ol_li = document.createElement('li');
+
+                        // TODO: reorganize in a dictionary and build a brief algorithm
+                        // Retrieve data to present
                         var content = values[key][table_key];
                         var degree = content['degree'];
                         var course = content['course'];
@@ -148,16 +150,11 @@ fetch('/assets/db/publications.json').then(
                         var doi_link = content['reference-doi-link'];
 
                         // Title and Badge
-                        var title_text_node = document.createTextNode(title);
                         var title_element = document.createElement('h4');
-                        var li_title = document.createElement('li');
-                        title_element.appendChild(title_text_node);
-                        li_title.appendChild(title_element);
-                        li_title.style.paddingBottom = '0.4em'; // TODO: improve, could be the opposite
+                        title_element.textContent = title;
                         
                         var badge_element = document.createElement('a');
-                        var badge_text_node = document.createTextNode(table_key);
-                        badge_element.appendChild(badge_text_node);
+                        badge_element.textContent = table_key;
                         title_element.appendChild(badge_element);
                         
                         if (table_key == 'phd-thesis') {     // Enum all the possible types
@@ -166,41 +163,40 @@ fetch('/assets/db/publications.json').then(
                         else if (table_key == 'masters-thesis') {
                             badge_element.classList.add('badge', 'masters-thesis');
                         }
-
+                        
+                        university_publications_ol_li.appendChild(title_element);
+                        
+                        // Create subelements structure of ol
+                        // reporting university publications information in an ul
+                        var university_publication_description_ul = document.createElement('ul');
+                        university_publication_description_ul.className = 'scientific-publication-details';
+                        university_publication_description_ul.id = 'university-pulication-details-ul';
+                        
                         // Degree, course, university and year setting
+                        var university_publication_description_ul_li = document.createElement('li');
                         var string_content = degree + ' in ' + course + ', ' + publisher + ', ' + year;
                         var degree_course_publisher_year_text_node = document.createTextNode(string_content);
-                        var li_degree_course_publisher_year = document.createElement('li');
-                        li_degree_course_publisher_year.appendChild(degree_course_publisher_year_text_node);
+                        university_publication_description_ul_li.appendChild(degree_course_publisher_year_text_node);
+                        university_publication_description_ul.appendChild(university_publication_description_ul_li);
                         
                         // Link setting
-                        var li_link = document.createElement('li');
-                        li_link.textContent = 'Link: ';
+                        var university_publication_description_ul_li = document.createElement('li');
+                        university_publication_description_ul_li.textContent = 'Link: ';
                         var link_a = document.createElement('a');
                         link_a.textContent = doi_link; 
                         link_a.href = doi_link;
                         link_a.target = '_blank';
                         link_a.rel = 'noopener noreferrer';
                         link_a.style.color = 'orange';
-                        li_link.appendChild(link_a);
-                        
-                        // Appending elements to appropriate lists and styling
-                        var unordered_artifact_description = document.createElement('ul');
-                        unordered_artifact_description.appendChild(li_degree_course_publisher_year);
-                        unordered_artifact_description.appendChild(li_link);
-                        unordered_artifact_description.style.marginLeft = '0.9em';
+                        university_publication_description_ul_li.appendChild(link_a);
 
-                        ordered_elements_list.appendChild(li_title);
-                        ordered_elements_list.appendChild(unordered_artifact_description);
-                        ordered_elements_list.style.paddingLeft = '1.1em';
-
-                    }
-                    // Appending elements to appropriate lists and styling
-                    university_thesis_section.appendChild(ordered_elements_list);
-                    university_thesis_section.style.paddingTop = '10px';
-                    university_thesis_section.style.paddingBottom = '10px';
-                    university_thesis_section.style.paddingLeft = '25px';
-                
+                        // Append the built li to the university_publication_description_ul
+                        university_publication_description_ul.appendChild(university_publication_description_ul_li);
+                        // Append the university_publication_description_ul to the upper-level li element in process
+                        university_publications_ol_li.appendChild(university_publication_description_ul);
+                        // Append the finally built university_publications_ol_li to the list grouping university articles
+                        university_publications_ol.appendChild(university_publications_ol_li);
+                    }                
                     break;
                 }
             }

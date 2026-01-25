74b75da2df11e9b4289566f12bdc2f44a11d17b7
Matteo Martinelli <wmatte2005@gmail.com>
2026-01-26 00:02:52 +0100

    Router adjusted
    The navigation has been setup with using hashlinks.

--- _mvc/app/router/Router.js
+++ _mvc/app/router/Router.js
@@ -10,55 +10,78 @@ export class Router {
     init() {
         console.log('Into Router init');
         // Initilize the route based on the current URL
-        // this.handleRoute(window.location.pathname);
+        // this.handleRoute(window.location.pathname); // Gets the current URL and calls handleRoute
+        this.handleRoute(window.location.hash.slice(1) || '/_mvc/index'); // Gets the current URL and calls handleRoute
 
-        // back/forward in the browser
-        window.addEventListener('popstate', () => {
-            this.handleRoute(window.location.pathname, {push: false});
-        });
+        // back/forward in the browser: when the forward/backward browser commands are pushed, the
+        // popstate event is fired. In this case, the Router navigates in the page got from the 
+        // event.
+        // window.addEventListener('popstate', (event) => {
+        //     console.log('popstate fired');
+        //     console.log('event.state:', event.state); // The state object from pushState
+        //     console.log('window.location.pathname:', window.location.pathname)
+        //     // If event.state is given, then the associated path is used for navigation; 
+        //     // Otherwise, the actual path stored and popped by the browser is used
+        //     const path = event.state?.path || window.location.pathname;
+        //     this.handleRoute(path);
+        // });
 
-        // delegation of clicks on links <a data-link>...</a>
+        // Listen for hash changes (browser back/forward)
+        window.addEventListener('hashchange', () => {
+            const path = window.location.hash.slice(1);
+            this.handleRoute(path);
+        })
+
+        // Delegation of clicks on links <a data-link>...</a>: here is added a listener to all clicks performed
+        // in the page. It listens to click for navigation. It is possible to move this centralized approach to 
+        // single Controllers in case of more granular-custom behaviour.
         document.addEventListener('click', (event) => {
+            // Getting the link element
             const link = event.target.closest('a[data-link]');
-            if (!link) return; 
-
-            event.preventDefault();
-            const href = link.getAttribute('href');
-            this.navigate(href);
+            // Checking its content; handled only if the event comes from a nav-bar button
+            if (!link) return;
+            if (link.getAttribute('class') === 'menu-button') {
+                event.preventDefault();
+                const href = link.getAttribute('href');
+                console.log('Handling the click event. Navigating to ', href);
+                this.navigate(href);
+            } 
+            else {
+                console.log('Click event from an element from a menu-button; handling skipped.')
+            }
         });
     }
 
     navigate(path) {
-        history.pushState({}, '', path);
-        this.handleRoute(path);
+        // When navigating from a link in the webpage, a new entry is added in the browser history
+        // history.pushState({ path }, '', path);
+        // this.handleRoute(path);
+        // setting the hash automatically triggers the 'hashchange' event defined in the init()
+        window.location.hash = path;
     }
 
     handleRoute(path) {
-        // this.currentController = new Controller(this.root, this);
-        // this.currentController.init();
-        if (window.location.pathname === path && this.currentController) {
-            console.log('Already in ', path);
-            return;
-        }
-
+        // Given the path, looks for the route in the routing table; 
+        // when found, instantiates the correct Controller; if not, fallbacks to home. 
         console.log('Looking for Route match from path ', path);
         const match = routes.find(r => r.path === path) || routes[0]; // fallback to home
+        const Controller = match.controller;
         console.log('Found match as ', match);
         
-        const Controller = match.controller;
-
-        // Deconstruct eventual previous controller
+        // Deconstructs the eventual actual controller
         if (this.currentController?.destroy) {
             this.currentController.destroy();
         }
 
-        // Clean the content
+        // Cleans the content
         this.root.innerHTML = '';
 
-        // Instantiate and initialize the new controller
+        // Instantiate and initialize the new Controller given the current location
         console.log('Instantiating new Controller from Router');
-        this.currentController = new Controller(this.rootSelector, this);
+        this.currentController = new Controller('#app', this);
+        if (this.currentController) {       // Controller initialization
+            this.currentController.init();
+        }
         console.log('Controller ', this.currentController, 'instantiated from Router');
-    
     }
 }

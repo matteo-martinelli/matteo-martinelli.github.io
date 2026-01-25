8f50e99804917cc2f63cdee6d8896e45ad16ca50
Matteo Martinelli <wmatte2005@gmail.com>
2026-01-26 00:00:22 +0100

    Entry point cleaned and summarized in a run function

--- _mvc/app.js
+++ _mvc/app.js
@@ -1,26 +1,13 @@
-// import { ProjectController } from './controllers/ProjectController.js';
-// import { includePartial } from './app/helpers/htmlInjector.js';
-import { HomeController } from "./app/controllers/HomeController.js";
 import { Router } from "./app/router/Router.js";
 
-console.log('app.js uploaded');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const router = new Router('body');
-  router.init();
-  const controller = new HomeController('body', router);
-  // controller.init();
-});
+function run() {
+  console.log('app.js uploaded');
 
-// document.addEventListener('DOMContentLoaded', async () => {
-//   await Promise.all([
-//     includePartial('body', '/_mvc/templates/low-navbar.html'),
-//     includePartial('body', '/_mvc/templates/home-top-content.html'),
-//     includePartial('body', '/_mvc/templates/spacer.html')
-//   ]);
-// });
+  document.addEventListener('DOMContentLoaded', () => {
+    const router = new Router('#app');
+    router.init();
+  });
+}
 
-// document.addEventListener('DOMContentLoaded', async () => {
-  // const projectController = new ProjectController();
-  // await projectController.init();
-// });
+run();

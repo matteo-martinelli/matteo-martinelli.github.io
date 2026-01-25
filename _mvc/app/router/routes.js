dda11f3659ff687405f00240021cb2ac9258bfa4
Matteo Martinelli <wmatte2005@gmail.com>
2026-01-25 23:59:41 +0100

    Routes adjusted in htmls and routes.js file
    Moreover, future routes have been added in the routes.js file as comments.

--- _mvc/app/router/routes.js
+++ _mvc/app/router/routes.js
@@ -1,17 +1,34 @@
 import { HomeController } from '../controllers/HomeController.js';
 import { BiographyController } from '../controllers/BiographyController.js';
 
+// TODO: when updating paths, do the following: 
+// 1. Change routes.js paths (e.g., to /bio).
+// 2. Update all href attributes in your HTML files (e.g., low-navbar.html).
+// 3. Update the default fallback path in Router.js (inside init()).
+
 export const routes = [
+  // {
+  //   path: '/',          // home
+  //   controller: HomeController
+  // },
   {
-    path: '/',          // home
+    path: '/_mvc/index',  // home page
     controller: HomeController
   },
   {
-    path: '/_mvc/index.html',  // home page
-    controller: HomeController
-  },
-  {
-    path: '/_mvc/bio.html',     // biografia
+    path: '/_mvc/bio',     // biografia
     controller: BiographyController
-  }
+  },
+  // {
+  //   path: '/publications',     // biografia
+  //   controller: PublicationsController
+  // }, 
+  // {
+  //   path: '/projects',     // biografia
+  //   controller: ProjectsController
+  // },
+  // {
+  //   path: '/training',     // biografia
+  //   controller: TrainingController
+  // }
 ];

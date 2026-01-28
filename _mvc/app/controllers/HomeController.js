ba31a713972d2800f45ee09aed9709051ef31a61
Matteo Martinelli <wmatte2005@gmail.com>
2026-01-28 19:32:21 +0100

    Added dynamic upload of home page style via HomeController

--- _mvc/app/controllers/HomeController.js
+++ _mvc/app/controllers/HomeController.js
@@ -4,6 +4,7 @@ export class HomeController {
     constructor(rootSelector, router) {
         this.view = new HomeView(rootSelector);
         this.router = router;
+        this.styleElement = null;
         console.log('Into Home Controller')
     }
     
@@ -13,6 +14,26 @@ export class HomeController {
             this.view.render(),
             this.view.includePartial('#layout-navbar', '/_mvc/templates/low-navbar.html'),
         ]);
+
+        // Loading the page CSS
+        this.styleElement = document.createElement('link');
+        this.styleElement.rel = 'stylesheet';
+        this.styleElement.href = '/assets/css/pages/index-style.css';
+        document.head.appendChild(this.styleElement);
+    }
+
+    destroy() {
+        // Destroy style
+        if (this.styleElement) {
+            this.styleElement.remove();
+            this.styleElement = null;
+        }
+
+        const headerContainer = document.getElementById('header');
+
+        if (headerContainer) {
+            headerContainer.innerHTML = '';
+        }
     }
 
 }

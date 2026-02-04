1ffb6700fd9a00e49aeb13679ff134766a008afb
Matteo Martinelli <wmatte2005@gmail.com>
2026-02-04 23:45:54 +0100

    Populated Projects MVC

--- _mvc/app/models/ProjectsModel.js
+++ _mvc/app/models/ProjectsModel.js
@@ -1,17 +1,44 @@
 export class ProjectsModel {
   constructor() {
-    this.projects = [];
+    this.industrialProjects = [];
+    this.scientificProjects = [];
   }
 
   async load() {
-    const response = await fetch('/_mvc/data/db/industrial-projects.json');
-    console.log(response);
+    this.loadIndustrialProjects();
+    this.loadScientificProjects();
+  }
+
+  async loadIndustrialProjects(path='/_mvc/data/db/industrial-projects.json') {
+    const response = await fetch(path);
+    console.log('Fetched response:', response);
     if (!response.ok) {
       throw new Error('There was an error reading the file!');
     }
     const json_projects = await response.json();
-    const raw_projects = json_projects[0];
-    this.projects.push(raw_projects);
+    console.log('Extracted response:', json_projects);
+    const raw_projects = json_projects['industrial-projects'];
+    // console.log('Industrial projects:', raw_projects);
+    for (var elem in raw_projects) {
+      this.industrialProjects.push(raw_projects[elem]);
+    }
+    // console.log(this.industrialProjects);
+  }
+
+  async loadScientificProjects(path='/_mvc/data/db/scientific-projects.json') {
+    const response = await fetch(path);
+    console.log('Fetched response:', response);
+    if (!response.ok) {
+      throw new Error('There was an error reading the file!');
+    }
+    const json_projects = await response.json();
+    console.log('Extracted response:', json_projects);
+    const raw_projects = json_projects['scientific-projects'];
+    // console.log('Scientific projects:', raw_projects);
+    for (var elem in raw_projects) {
+      this.scientificProjects.push(raw_projects[elem]);
+    }
+    // console.log(this.scientificProjects);
   }
 
   getAll() {

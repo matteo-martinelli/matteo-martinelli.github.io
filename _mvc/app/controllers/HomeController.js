086cb816d1e313ad585828ff0648731425b115c9
Matteo Martinelli <wmatte2005@gmail.com>
2026-02-07 16:59:45 +0100

    Added a TODO

--- _mvc/app/controllers/HomeController.js
+++ _mvc/app/controllers/HomeController.js
@@ -1,5 +1,7 @@
 import { HomeView } from "../views/HomeView.js";
 
+
+// TODO: move all the parts inserting HTML here and there in the view section.
 export class HomeController {
     constructor(rootSelector, router) {
         this.view = new HomeView(rootSelector);

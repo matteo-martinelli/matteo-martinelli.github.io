536392e0a7935c6dc755e1e5ad60cc62b0adea06
Matteo Martinelli <wmatte2005@gmail.com>
2026-02-14 19:29:28 +0100

    Old website folder version deleted
    Migration towards MVC structure completed.

--- _old/assets/js/config.js
+++
@@ -1,5 +0,0 @@
-//File untracked by Git
-
-var config = {
-    MY_MAIL : 'matteo.martinelli.1991@gmail.com',
-}
--- _old/assets/js/projects/contact-alert.js
+++
@@ -1,8 +0,0 @@
-// Alert for the e-mail link in the text corpus
-
-const MAIL = "matteo.martinelli.1991@gmail.com";
-const contactMeLink = document.querySelector("#contact-form-link");
-
-contactMeLink.addEventListener('click', function(event){
-    alert('Write me at ' + MAIL + "!");
-})
